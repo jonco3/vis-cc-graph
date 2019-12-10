a36b681b44a97ba327da017c64f5d8dd3814ed22
Jon Coppeard <jonco@pobox.com>
2019-12-10 13:18:48 +0000

    Refactor selection a little

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
  * Visualise cycle collector graphs.
  */
 
-let data = [];
+let data;
 
 const inputURL = "data.log";
 
@@ -131,14 +131,15 @@ function parseLog(data) {
 function update() {
   let filter = document.getElementById("filter").value;
   let depth = parseInt(document.getElementById("depth").value);
+  selectNodes(data, filter, depth);
   display(data, filter, depth);
 }
 
 function display(nodeMap, filter, depth) {
-  let count = selectNodes(nodeMap, filter, depth);
   let nodeList = Array.from(nodeMap.values()).filter(d => d.selected);
   let links = getLinks(nodeMap);
 
+  let count = nodeList.length;
   setStatus(`Displaying ${count} out of ${nodeMap.size} nodes`);
 
   let width = Math.max(Math.sqrt(count) * 120, 800);
@@ -196,7 +197,7 @@ function display(nodeMap, filter, depth) {
   let simulation = d3.forceSimulation()
       .force("link", d3.forceLink().id(function(d) { return d.id; }).distance(50))
       .force("charge", d3.forceManyBody().strength(-1))
-      .force("collision", d3.forceCollide().radius(50))
+      .force("collision", d3.forceCollide().radius(30))
       .force("center", d3.forceCenter(width / 2, height / 2));
 
   simulation
@@ -247,24 +248,27 @@ function display(nodeMap, filter, depth) {
 }
 
 function selectNodes(nodeMap, filter, maxDepth) {
+  if (!filter) {
+    filter = "";
+  }
+
   if (!maxDepth || Number.isNaN(maxDepth)) {
     maxDepth = 0;
   }
 
-  let count = 0;
-  let worklist = [];
-  for (let d of nodeMap.values()) {
-    d.selected = !filter || d.name.includes(filter);
-    if (d.selected) {
-      count++;
-      if (filter) {
-        worklist.push({node: d, depth: 0});
-      }
+  if (!filter) {
+    for (let d of nodeMap.values()) {
+      d.selected = true;
     }
+    return;
   }
 
-  if (!filter) {
-    return count;
+  let worklist = [];
+  for (let d of nodeMap.values()) {
+    d.selected = d.name.includes(filter);
+    if (d.selected) {
+      worklist.push({node: d, depth: 0});
+    }
   }
 
   while (worklist.length) {
@@ -279,14 +283,11 @@ function selectNodes(nodeMap, filter, maxDepth) {
         throw `Missing node {id}`;
       }
       if (!node.selected) {
-        count++;
         node.selected = true;
         worklist.push({node: node, depth: depth});
       }
     }
   }
-
-  return count;
 }
 
 function getLinks(objects) {
